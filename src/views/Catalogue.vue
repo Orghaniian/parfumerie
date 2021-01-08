dac0bdc9e797a258e8fac6009526d00e1ad105a2
Orghaniian <e163100@univ-lemans.fr>
2021-01-08 20:05:51 +0100

    ajout barre de rehcerche

--- src/views/Catalogue.vue
+++ src/views/Catalogue.vue
@@ -1,9 +1,11 @@
 <template>
-  Catalogue
+  <h2>Catalogue</h2>
+  <label>Rechercher: </label>
+  <input type="text" v-model="nom" @input="test" />
+  <p>Resultat(s) pour: {{ nom }}</p>
   <div v-for="article in articles" :key="article.id">
     <pre>{{ article }}</pre>
   </div>
-  <input type="number" v-model="limit" />
 </template>
 
 <script>
@@ -12,16 +14,25 @@ export default {
   name: "Catalogue",
   setup() {
     const articles = ref([]);
-    // const db = connectDb;
-    // console.log(db);
+    const nom = ref("");
 
-    onMounted(() => {
-      fetch("http://localhost:4040/articles").then((response) => {
+    const load = function () {
+      let query = "http://localhost:4040/articles";
+      if (nom.value) query += `?nom=${nom.value}`;
+      fetch(query).then((response) => {
         response.json().then((data) => (articles.value = data.data));
       });
+    };
+
+    const test = function () {
+      load();
+    };
+
+    onMounted(() => {
+      load();
     });
 
-    return { articles };
+    return { articles, nom, test };
   },
 };
 </script>
