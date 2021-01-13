8ce4a9e8195263c7a884be379dfd475a649e1ac4
Orghaniian <e163100@univ-lemans.fr>
2021-01-13 17:47:35 +0100

    Ajouter bouton addCart

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -2,10 +2,14 @@
   Article: {{ no }}
   <p v-if="article">{{ article }}</p>
   <p v-else>Chargement...</p>
+  <label for="quantite"></label>
+  <input type="number" min="0"  id="quantite" v-model="quantite"/>
+  <button @click.prevent="addCart">{{ article ? "Ajouter au panier" : "Chargement..." }}</button>
 </template>
 
 <script>
 import {onMounted, ref} from "vue";
+import useCart from "@/utils/useCart"
 
 export default {
   name: "Article",
@@ -15,11 +19,31 @@ export default {
 
     onMounted(() => {
       fetch("http://localhost:4040/article/" + props.no).then((response) => {
-        response.json().then((data) => (article.value = data.data));
+        response.json().then((data) => article.value = data.data);
       })
     })
 
-    return { article }
+    const { panier, setCart } = useCart();
+    const quantite = ref(0)
+
+    const addCart = function (){
+      if(typeof quantite.value === "string")
+        quantite.value = parseInt(quantite.value)
+      if(quantite.value > 0){
+        let alreadyIn = false
+        panier.value.forEach(article => {
+          if( article.no === props.no){
+            article.quantite += quantite.value
+            alreadyIn = true
+          }
+        })
+        if(!alreadyIn)
+          panier.value.push({ no: props.no, quantite: quantite.value })
+        setCart(panier.value)
+      }
+    }
+
+    return { article, addCart, quantite }
   }
 }
 </script>
