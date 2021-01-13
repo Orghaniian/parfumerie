d0aa2c444813d976006ddaa268870dd9aeb4cfd6
Orghaniian <e163100@univ-lemans.fr>
2021-01-13 17:48:27 +0100

    ajout profil

---
+++ src/views/Profil.vue
@@ -0,0 +1,25 @@
+<template>
+  {{ infos }}
+</template>
+
+<script>
+import {onMounted, ref} from "vue";
+
+export default {
+  name: "Profil",
+  setup () {
+    const infos = ref(null)
+
+    onMounted(() => {
+      fetch("http://localhost:4040/client/" + JSON.parse(localStorage.getItem("utilisateur")).CodeClient).then((response) => {
+        response.json().then((data) => (infos.value = data.data));
+      })
+    })
+    return { infos }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
