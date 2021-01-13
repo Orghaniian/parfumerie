f2a9e4f93ce40b7b9589680a5812e2a1df9c80ce
Orghaniian <e163100@univ-lemans.fr>
2021-01-13 19:37:09 +0100

    Création du composant card pour articles et clients

---
+++ src/components/Card.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="card">
+    <slot name="img"/>
+    <div class="content">
+      <slot/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+name: "Card"
+}
+</script>
+
+<style lang="scss">
+.card{
+  display: grid;
+  grid-template-columns: 1fr 1.2fr;
+  padding: 15px;
+  margin: 36px 20px;
+  width: 400px;
+  height: 190px;
+  background: linear-gradient(180deg, #A79FDB 0%, rgba(159, 146, 237, 0.08) 100%);
+  border-radius: 10px;
+}
+img{
+  width: 160px;
+  height: 160px;
+  border-radius: 10px;
+  align-self: center;
+}
+</style>
--- src/components/CatalogueItem.vue
+++ src/components/CatalogueItem.vue
@@ -1,19 +1,25 @@
 <template>
-  <div class="article">
-    <img src="https://www.marionnaud.fr/medias/Eau-de-Parfum-101065090.jpg?context=bWFzdGVyfGZyb250L2V4dHJhem9vbXwxNDg2Nzl8aW1hZ2UvanBlZ3xmcm9udC9leHRyYXpvb20vaDEyL2g3OC8xMDE4ODg0NzIxODcxOC5qcGd8YmE1YTI3NDg4MGRkOTcxMWY0ZTFlNDk4YTU5MzM0OGY0ODhkYjg1NWEwZjRkMGQxODgwZDg0MzMxNWFmNjM5ZA" alt="">
-    <div class="desc">
-      <div>
-        <h3>{{ article.Nom }}</h3>
-        <p>{{ article.Prix_unitaire }} Points</p>
+  <Card>
+    <template v-slot:img>
+      <img src="https://www.marionnaud.fr/medias/Eau-de-Parfum-101065090.jpg?context=bWFzdGVyfGZyb250L2V4dHJhem9vbXwxNDg2Nzl8aW1hZ2UvanBlZ3xmcm9udC9leHRyYXpvb20vaDEyL2g3OC8xMDE4ODg0NzIxODcxOC5qcGd8YmE1YTI3NDg4MGRkOTcxMWY0ZTFlNDk4YTU5MzM0OGY0ODhkYjg1NWEwZjRkMGQxODgwZDg0MzMxNWFmNjM5ZA" alt="">
+    </template>
+    <template v-slot:default>
+      <div class="desc">
+        <div>
+          <h3>{{ article.Nom }}</h3>
+          <p>{{ article.Prix_unitaire }} Points</p>
+        </div>
+        <router-link :to="{ name: 'Article', params: { no: article.No_article } } "><span>Consulter</span></router-link>
       </div>
-      <router-link :to="{ name: 'Article', params: { no: article.No_article } } "><span>Consulter</span></router-link>
-    </div>
-  </div>
+    </template>
+  </Card>
 </template>
 
 <script>
+import Card from "@/components/Card";
 export default {
   name: "CatalogueItem",
+  components: {Card},
   props: {
     article: Object
   },
@@ -23,21 +29,12 @@ export default {
 </script>
 
 <style lang="scss">
-.article{
-  display: grid;
-  grid-template-columns: 1fr 1.2fr;
-  padding: 15px;
-  margin: 36px 20px;
-  width: 400px;
-  height: 190px;
-  background: linear-gradient(180deg, #A79FDB 0%, rgba(159, 146, 237, 0.08) 100%);
-  border-radius: 10px;
-
   .desc{
     display: grid;
-    grid-auto-rows: 1fr 20%;
+    grid-template-rows: 1fr 20%;
     font-size: 1.25em;
     position: relative;
+    height: 100%;
 
     div{
       display: grid;
@@ -70,7 +67,6 @@ export default {
       line-height: 23px; // centre verticalement
       justify-self: center;
     }
-  }
   img{
     width: 160px;
     height: 160px;
