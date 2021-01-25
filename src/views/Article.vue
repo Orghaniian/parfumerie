16f31ae63b24a0c1363c7db43da9576316a63ba4
Orghaniian <e163100@univ-lemans.fr>
2021-01-25 18:54:50 +0100

    Vérification du stock

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -1,10 +1,16 @@
 <template>
   Article: {{ no }}
-  <p v-if="article">{{ article }}</p>
+  <div v-if="article">
+    <pre>{{ article }}</pre>
+    <p v-if="article.Quantite_en_stock <= 0">Stock épuisé!</p>
+    <label for="quantite">Quantité: </label>
+    <input type="number" min="0"  id="quantite" v-model="quantite"/>
+    <button :disabled="article.Quantite_en_stock <= 0" @click.prevent="addCart">{{ article ? "Ajouter au panier" : "Chargement..." }}</button>
+  </div>
   <p v-else>Chargement...</p>
-  <label for="quantite"></label>
-  <input type="number" min="0"  id="quantite" v-model="quantite"/>
-  <button @click.prevent="addCart">{{ article ? "Ajouter au panier" : "Chargement..." }}</button>
+  <p v-if="erreur">{{ erreur }}</p>
+
+
 </template>
 
 <script>
@@ -29,25 +35,32 @@ export default {
 
     const { panier, setCart } = useCart();
     const quantite = ref(0)
+    const erreur = ref(null)
 
     const addCart = function (){
+      erreur.value = null
       if(typeof quantite.value === "string")
         quantite.value = parseInt(quantite.value)
       if(quantite.value > 0){
-        let alreadyIn = false
-        panier.value.forEach(article => {
-          if( article.no === props.no){
-            article.quantite += quantite.value
-            alreadyIn = true
-          }
-        })
-        if(!alreadyIn)
-          panier.value.push({ no: props.no, quantite: quantite.value })
-        setCart(panier.value)
-      }
+        if(quantite.value <= article.value.Quantite_en_stock){
+          let alreadyIn = false
+          panier.value.forEach(article => {
+            if( article.no === props.no){
+              article.quantite += quantite.value
+              alreadyIn = true
+            }
+          })
+          if(!alreadyIn)
+            panier.value.push({ no: props.no, quantite: quantite.value })
+          setCart(panier.value)
+        }
+        else erreur.value = `Il ne reste que ${article.value.Quantite_en_stock} exemplaires en stock`
+      }else erreur.value = "Veuillez selectionner une quantité supérieur à 0"
+
+
     }
 
-    return { article, addCart, quantite }
+    return { article, addCart, quantite, erreur }
   }
 }
 </script>
