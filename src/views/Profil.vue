4b8e992ceb1cf34bfbdef829450858f1c35f865a
Orghaniian <e163100@univ-lemans.fr>
2021-02-03 17:51:23 +0100

    Affichage de l'historique des commandes

--- src/views/Profil.vue
+++ src/views/Profil.vue
@@ -1,5 +1,11 @@
 <template>
-  {{ infos }}
+  <div>
+    {{ infos }}
+  </div>
+  <h3>Historique des commandes</h3>
+  <div v-for="commande in commandes" :key="commande.No_commande">
+    {{ commande }}
+  </div>
 </template>
 
 <script>
@@ -10,16 +16,21 @@ export default {
   setup () {
     document.title = `Profil`
     const infos = ref(null)
+    const commandes = ref(null)
 
     onMounted(() => {
       fetch("http://localhost:4040/client/" + JSON.parse(localStorage.getItem("utilisateur")).CodeClient).then((response) => {
         response.json().then((data) => {
           infos.value = data.data
           document.title = `Profil - ${infos.value.Nom}`
+          fetch(`http://localhost:4040/commandes/client/${infos.value.Code_client}`).then(response => response.json().then(data => {
+            commandes.value = data.data
+          }))
         });
       })
+
     })
-    return { infos }
+    return { infos, commandes }
   }
 }
 </script>
